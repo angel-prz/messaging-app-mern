b3a20df74c876b28e2d0d98e9582cb2f0d831bd8
angel-prz <191927151+angel-prz@users.noreply.github.com>
2026-04-22 21:37:29 -0300

    refactor: add componente Chat

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { Avatar, IconButton } from "@mui/material";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SearchIcon from "@mui/icons-material/Search";
+import "./Chat.css";
+const Chat = () => {
+  const [seed, setSeed] = useState("");
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar
+          src={`https://api.dicebear.com/9.x/toon-head/svg?flip=true&seed=${seed}`}
+        />
+        <div className="chat__headerInfo">
+          <h3>Nome da sala</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body"></div>
+      <div className="chat__footer"></div>
+    </div>
+  );
+};
+export default Chat;
