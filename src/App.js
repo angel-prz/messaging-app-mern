99e954ab14cbb5124eaf1048a8d2d38ef75949fd
angel-prz <191927151+angel-prz@users.noreply.github.com>
2026-04-22 22:39:12 -0300

    feat: add hook com axios GET para /messagesßync

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Chat from "./components/Chat/Chat";
+import axios from "./components/axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    axios.get("/messages/sync").then((res) => {
+      setMessages(res.data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <div className="app__body">
